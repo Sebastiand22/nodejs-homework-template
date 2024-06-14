b476262c0e5d4e4ca82fb4ea4d9dbf953022f134
Sebastiand22 <sebastiandunduc16@gmail.com>
2024-06-14 20:59:30 +0300

    first commit

---
+++ db.mjs
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const uri = process.env.DATABASE;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('Database connection successful');
+  } catch (error) {
+    console.error('Database connection error:', error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
